5e18694a9bf938bf5164725fc31ddb6b1177bc96
Mohau Mushi <david1mushi1@gmail.com>
2024-05-30 14:45:09 +0200

    Indented Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import React from "react"
-import Header from "./components/Header"
-import Meme from "./components/Meme"
+import React from "react";
+import Header from "./components/Header";
+import Meme from "./components/Meme";
 
 function App() {
-
   return (
     <>
-     <div>
-      <Header />
-      <Meme />
-     </div>
+      <div>
+        <Header />
+        <Meme />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
